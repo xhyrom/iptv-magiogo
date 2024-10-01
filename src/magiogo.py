59d666103eaf87091068524503c4e62b14464f5b
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-10-01 18:49:28 +0200

    feat: xmltv

--- src/magiogo.py
+++ src/magiogo.py
@@ -1,6 +1,6 @@
 # Taken from https://github.com/Saros72/IPTV-Web-Server/blob/main/providers/magio/magio.py
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 import json
 from urllib.parse import urlparse
@@ -180,3 +180,167 @@ def get_catchup(id, utc, utcend):
 
     # Return fallback URL if any request fails
     return "http://sledovanietv.sk/download/noAccess-sk.m3u8"
+
+
+def get_epg(channels, from_date, to_date):
+    from_date = from_date.replace(hour=0, minute=0, second=0, microsecond=0)
+    to_date = to_date.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(
+        days=1
+    )
+    now = datetime.utcnow()
+
+    days = int((to_date - from_date).days)
+
+    accesstoken, refreshtoken = get_data()
+    params = {"refreshToken": refreshtoken}
+    headers = {
+        "Origin": "https://www.magiogo.sk",
+        "Pragma": "no-cache",
+        "Referer": "https://www.magiogo.sk/",
+        "User-Agent": USER_AGENT,
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site",
+    }
+
+    # Request new tokens
+    req = requests.post(
+        "https://skgo.magio.tv/v2/auth/tokens", json=params, headers=headers
+    ).json()
+    if req["success"]:
+        accesstoken = req["token"]["accessToken"]
+        refreshtoken = req["token"]["refreshToken"]
+        set_data({"accesstoken": accesstoken, "refreshtoken": refreshtoken})
+
+    headers["authorization"] = "Bearer " + accesstoken
+
+    result = {}
+
+    for n in range(days):
+        current_day = from_date + timedelta(n)
+
+        filter = "startTime=ge=%sT00:00:00.000Z;startTime=le=%sT00:59:59.999Z" % (
+            current_day.strftime("%Y-%m-%d"),
+            (current_day + timedelta(days=1)).strftime("%Y-%m-%d"),
+        )
+
+        fetch_more = True
+        offset = 0
+        while fetch_more:
+            params = {
+                "name": DEVICE_NAME,
+                "devtype": DEVICE_TYPE,
+                "id": id,
+                "prof": "p5",
+                "ecid": "",
+                "drm": "verimatrix",
+                "lang": "SK",
+                "offset": offset * 20,
+                "list": "LIVE",
+                "filter": filter,
+            }
+
+            # Request EPG
+            req = requests.get(
+                "https://skgo.magio.tv/v2/television/epg",
+                params=params,
+                headers=headers,
+            ).json()
+
+            fetch_more = len(req["items"]) == 20
+            offset = offset + 1
+
+            for i in req["items"]:
+                for p in i["programs"]:
+                    channel = str(p["channel"]["id"])
+
+                    if channel not in channels:
+                        continue
+
+                    if channel not in result:
+                        result[channel] = []
+
+                    programme = _programme_data(p["program"])
+                    programme.start_time = datetime.utcfromtimestamp(
+                        p["startTimeUTC"] / 1000
+                    )
+                    programme.end_time = datetime.utcfromtimestamp(
+                        p["endTimeUTC"] / 1000
+                    )
+                    programme.duration = p["duration"]
+                    programme.is_replyable = (
+                        programme.start_time > (now - timedelta(days=7))
+                    ) and (programme.end_time < now)
+
+                    result[channel].append(programme)
+
+    return result
+
+
+class Base:
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+class Programme(Base):
+    def __init__(self):
+        self.id = ""  # type: str
+        # Programme Start Time in UTC
+        self.start_time = None  # type: datetime or None
+        # Programme End Time in UTC
+        self.end_time = None  # type: datetime or None
+        self.title = ""
+        self.description = ""
+        self.thumbnail = ""
+        self.poster = ""
+        self.duration = 0
+        self.genres = []  # type: List[str]
+        self.actors = []  # type: List[str]
+        self.directors = []  # type: List[str]
+        self.writers = []  # type: List[str]
+        self.producers = []  # type: List[str]
+        self.seasonNo = None
+        self.episodeNo = None
+        self.year = None  # type: int or None
+        self.is_replyable = False
+        # programme metadata
+        self.metadata = {}  # type: Dict[str, int]
+
+
+def _programme_data(pi):
+    def safe_int(value, default=None):
+        try:
+            return int(value)
+        except (ValueError, TypeError):
+            return default
+
+    programme = Programme()
+    programme.id = pi["programId"]
+    programme.title = pi["title"]
+    programme.description = "%s\n%s" % (
+        pi["episodeTitle"] or "",
+        pi["description"] or "",
+    )
+
+    pv = pi["programValue"]
+    if pv["episodeId"] is not None:
+        programme.episodeNo = safe_int(pv["episodeId"])
+    if pv["seasonNumber"] is not None:
+        programme.seasonNo = safe_int(pv["seasonNumber"])
+    if pv["creationYear"] is not None:
+        programme.year = safe_int(pv["creationYear"])
+    for i in pi["images"]:
+        programme.thumbnail = i
+        break
+    for i in pi["images"]:
+        if "_VERT" in i:
+            programme.poster = i
+            break
+    for d in pi["programRole"]["directors"]:
+        programme.directors.append(d["fullName"])
+    for a in pi["programRole"]["actors"]:
+        programme.actors.append(a["fullName"])
+    if pi["programCategory"] is not None:
+        for c in pi["programCategory"]["subCategories"]:
+            programme.genres.append(c["desc"])
+
+    return programme
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from login import register_device
-from magiogo import get_channels, get_stream, get_catchup
+from magiogo import get_channels, get_stream, get_catchup, get_epg
+from common import parse_season_number
 
 import os
-from datetime import datetime
+import xmltv
+from datetime import datetime, timedelta
 
 from bottle import route, redirect, response, request, template, run
 
@@ -41,6 +43,77 @@ def magio_playlist():
         t = "#EXTM3U\n" + t
     response.content_type = "text/plain; charset=UTF-8"
     return t
+
+
+@route("/service/epg")
+def magio_epg():
+    date_from = datetime.now() - timedelta(days=0)
+    date_to = datetime.now() + timedelta(
+        days=int(os.environ.get("MAGIO_GUIDE_DAYS", 7))
+    )
+
+    channels = get_channels()
+    channel_ids = list(channels.keys())
+    epg = get_epg(channel_ids, date_from, date_to)
+
+    writer = xmltv.Writer(
+        date=datetime.now().strftime("%Y%m%d%H%M%S"),
+        generator_info_name="MagioGoIPTVServer",
+        generator_info_url="",
+        source_info_name="Magio GO Guide",
+        source_info_url="https://skgo.magio.tv/v2/television/epg",
+    )
+
+    for id, channel in channels.items():
+        channel_dict = {
+            "display-name": [(channel["name"], "sk")],
+            "icon": [{"src": channel["logo"]}],
+            "id": id,
+        }
+        writer.addChannel(channel_dict)
+
+    for channel_id, programmes in epg.items():
+        for programme in programmes:
+            programme_dict = {
+                "category": [(genre, "en") for genre in programme.genres],
+                "channel": channel_id,
+                "credits": {
+                    "producer": [producer for producer in programme.producers],
+                    "actor": [actor for actor in programme.actors],
+                    "writer": [writer for writer in programme.writers],
+                    "director": [director for director in programme.directors],
+                },
+                "date": str(programme.year),
+                "desc": [(programme.description, "")],
+                "icon": [{"src": programme.poster}, {"src": programme.thumbnail}],
+                "length": {"units": "seconds", "length": str(programme.duration)},
+                "start": programme.start_time.strftime("%Y%m%d%H%M%S"),
+                "stop": programme.end_time.strftime("%Y%m%d%H%M%S"),
+                "title": [(programme.title, "")],
+            }
+
+            # Define episode info only if provided
+            if programme.episodeNo is not None:
+                # Since seasonNo seems to be always null, try parsing the season from the title (e.g. Kosti X. = 10)
+                if programme.seasonNo is None:
+                    (show_title_sans_season, programme.seasonNo) = parse_season_number(
+                        programme.title
+                    )
+                    programme_dict["title"] = [(show_title_sans_season, "")]
+
+                programme_dict["episode-num"] = [
+                    (
+                        f"{(programme.seasonNo or 1) - 1} . {(programme.episodeNo or 1) - 1} . 0",
+                        "xmltv_ns",
+                    )
+                ]
+
+            writer.addProgramme(programme_dict)
+
+    writer.write(".epg.xmltv", True)
+
+    response.content_type = "application/xml"
+    return open(".epg.xmltv", "r").read()
 
 
 @route("/")

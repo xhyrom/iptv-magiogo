c4ebf01e1967ad939eec3ec0f0fc1df73e0bcc98
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-20 22:57:23 +0100

    refactor: remove print

--- src/magiogo.py
+++ src/magiogo.py
@@ -61,7 +61,6 @@ def get_stream(id):
             req = requests.get(url, headers=headers, allow_redirects=False)
             return req.headers["location"]
         else:
-            print(req)
             return "http://sledovanietv.sk/download/noAccess-sk.m3u8"
     else:
         return "http://sledovanietv.sk/download/noAccess-sk.m3u8"
